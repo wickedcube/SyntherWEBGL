031a12cf4d7e941734f2c2bfaa4dd88fe77cf019
wickedcube <wickedcube@gmail.com>
2020-03-15 19:24:56 +0530

    Init Commit

---
+++ StreamingAssets/sample.js
@@ -0,0 +1,61 @@
+(function () {
+
+    function login(callback) {
+        var CLIENT_ID = '529143f2174b4352afc00dfd7877c549';
+        var REDIRECT_URI = 'https://wickedcube.github.io/';
+        function getLoginURL(scopes) {
+            return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+                '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+                '&scope=' + encodeURIComponent(scopes.join(' ')) +
+                '&response_type=token';
+        }
+
+        var url = getLoginURL([
+            'user-read-email'
+        ]);
+
+        var width = 450,
+            height = 730,
+            left = (screen.width / 2) - (width / 2),
+            top = (screen.height / 2) - (height / 2);
+
+        window.addEventListener("message", function (event) {
+            var hash = JSON.parse(event.data);
+            if (hash.type == 'access_token') {
+                console.log(hash.access_token);
+                callback(hash.access_token);
+            }
+        }, false);
+
+        var w = window.open(url,
+            'Spotify',
+            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+        );
+
+    }
+
+    function getUserData(accessToken) {
+        return $.ajax({
+            url: 'https://api.spotify.com/v1/me',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            }
+        });
+    }
+
+    var templateSource = document.getElementById('result-template').innerHTML,
+        template = Handlebars.compile(templateSource),
+        resultsPlaceholder = document.getElementById('result'),
+        loginButton = document.getElementById('btn-login');
+
+    loginButton.addEventListener('click', function () {
+        login(function (accessToken) {
+            getUserData(accessToken)
+                .then(function (response) {
+                    loginButton.style.display = 'none';
+                    resultsPlaceholder.innerHTML = template(response);
+                });
+        });
+    });
+
+})();
